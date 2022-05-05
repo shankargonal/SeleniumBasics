28f6513feeaa44364c545c614afa598d510b4f79
shankargonal <shankargonal@gmail.com>
2022-05-05 07:41:38 +0530

    Added ONe Test cases

---
+++ Selenium_Basics/src/test/java/com/test/Testing_SampleLatercandeleteit.java
@@ -0,0 +1,34 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testing_SampleLatercandeleteit {
+
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.com/");
+		*/
+		
+		
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.get("https://google.com/");
+		driver.findElement(By.name("q")).sendKeys("mvnrepository");
+		driver.findElement(By.name("btnK")).click();
+		Thread.sleep(1000);
+		
+	}
+		
+}
